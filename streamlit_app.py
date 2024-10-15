13a21b5b797d14fa68f7f765eb267eb5ceeb223a
TCM25 <tcm@nitrovolt.com>
2024-10-15 16:09:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from PIL import Image
-import streamlit.components.v1 as components
-from streamlit_plotly_events import plotly_events
 
 st.set_page_config(page_title="Electrolyzer Data Analysis", page_icon=":bar_chart:", layout="wide")
 
@@ -11,12 +9,9 @@ image = Image.open('NitroVolt_Default(2).png')
 
 st.image(image, width=500)
 
-Title = st.title("Electrolyzer Data Analysis")
+Title = st.title("Electrolyzer Database")
 
-st.header("Suggestions for new electrolyzers")
-
-st.write("Welcome to our page! This online tool is a side project by NitroVolt, a Danish startup dedicated to revolutionizing sustainable ammonia production. We value your feedback and are committed to continuously improving and expanding our database. If you have any corrections or would like to suggest a new electrolyzer, please provide us with a datasheet and relevant links.")
-st.markdown("https://docs.google.com/spreadsheets/d/1X9WFXng7z1fJmlgLHs0YCmQAzgnc_U9PseOId34QzHk/edit?usp=sharing")
+st.write("Welcome to the Electrolyzer Database, a comprehensive survey of electrolyzers on the market! You can consume the information via a GoogleSheet [Database](https://docs.google.com/spreadsheets/d/1X9WFXng7z1fJmlgLHs0YCmQAzgnc_U9PseOId34QzHk/edit?usp=sharing) as well as via our summary plots on this page. This online tool is a side project by NitroVolt, a Danish startup dedicated to revolutionizing sustainable ammonia production via a novel electrochemical process. We value your feedback and are committed to continuously improving and expanding our database. If you have any corrections or would like to suggest a new electrolyzer add a comment in our spreadsheet or reach out to us via the contact form at the end of this page. In case you want to suggest the addition of a missing electrolyzer, please provide us with a datasheet and relevant links")
 
 sheet_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRRBGdnCxVwSG6HoCNVI1inzyAFjEYtccE-0OJojOUOwSqa2V82cpTZ4lhWYszI3kqYNRxs5xa4TO7O/pub?output=csv'
 df = pd.read_csv(sheet_url, header=1)
@@ -26,6 +21,9 @@ df['net/nominal production rate max'] = pd.to_numeric(df['net/nominal production
 df['average power consumption by stack min'] = pd.to_numeric(df['average power consumption by stack min'], errors='coerce')
 df['average power consumption by stack max'] = pd.to_numeric(df['average power consumption by stack max'], errors='coerce')
 df['average power consumption by system'] = pd.to_numeric(df['average power consumption by system'], errors='coerce')
+
+# Filter out rows where technology is "membrane free"
+df = df[df['technology'] != 'membrane free']
 
 # Load the sheet and convert 'reference' to string for URLs
 df['reference'] = df['reference'].astype(str)
@@ -85,38 +83,14 @@ title_font_size = st.sidebar.slider("Title Font Size", 15, 40, 25)
 axis_font_size = st.sidebar.slider("Axis Font Size", 10, 25, 16)
 legend_font_size = st.sidebar.slider("Legend Font Size", 10, 25, 22)
 
-# Scatter plot for stack
-fig_stack = px.scatter(
-    df_filtered_stack,
-    x='net/nominal production rate max',
-    y='average power consumption by stack combined',
-    color='technology',
-    hover_data=['manufacturer', 'technology', 'Location'],
-    custom_data=['reference'],
-    title='Net Production Rate vs Avg Power Consumption by Stack',
-    labels={
-        'net/nominal production rate max': 'Net production rate (Nm³/h)',
-        'average power consumption by stack combined': 'Avg Power Consumption by Stack (kWh/Nm³)'
-    },
-    template='plotly_white',
-    height=1050,  
-    width=1050    
-)
 
-fig_stack.update_layout(
-    xaxis=dict(range=[-10, net_prod_max], title_font=dict(size=axis_font_size), tickfont=dict(size=axis_font_size)),
-    yaxis=dict(range=[2.9, power_max], title_font=dict(size=axis_font_size), tickfont=dict(size=axis_font_size)),
-    legend=dict(title="Technology", title_font=dict(size=legend_font_size), font=dict(size=legend_font_size)),
-    title_font=dict(size=title_font_size)
-)
 
 fig_stack = px.scatter(
     df_filtered,
     x='net/nominal production rate max',
     y='average power consumption by stack combined',
     color='technology',
-    hover_data=['manufacturer', 'technology', 'Location'],  # Remove reference from hover data
-    custom_data=['reference'],  # Keep reference in customdata for redirection
+    hover_data=['manufacturer', 'technology', 'Location'], 
     title='Net Production Rate vs Avg Power Consumption by Stack',
     labels={
         'net/nominal production rate max': 'Net production rate (Nm³/h)',
@@ -127,15 +101,19 @@ fig_stack = px.scatter(
     width=1050
 )
 
-# Capture the click event on the chart using plotly_events
-clicked_points = plotly_events(fig_stack, click_event=True, hover_event=False, select_event=False, override_height=1050, override_width=1050)
+fig_stack.update_layout(
+    xaxis=dict(range=[-10, net_prod_max], title_font=dict(size=axis_font_size), tickfont=dict(size=axis_font_size)),
+    yaxis=dict(range=[2.9, power_max], title_font=dict(size=axis_font_size), tickfont=dict(size=axis_font_size)),
+    legend=dict(title="Technology", title_font=dict(size=legend_font_size), font=dict(size=legend_font_size)),
+    title_font=dict(size=title_font_size)
+)
 
-# If a point is clicked, redirect to the corresponding link
-if clicked_points:
-    reference_link = clicked_points[0]['customdata'][0]  # Get the reference link from the custom data
-    st.write(f"Redirecting to: {reference_link}")
-    st.write(f"[Open link]({reference_link})")
-    st.experimental_rerun()
+fig_stack.update_traces(marker=dict(size=marker_size), 
+    hovertemplate='<b>Electrolyzer technology</b>: %{customdata[1]}<br>' + 
+                  '<b>Net production rate</b>: %{x} Nm³/h<br>' + 
+                  '<b>System average power consumption</b>: %{y} kWh/Nm³<br>' + 
+                  '<b>Manufacturer</b>: %{customdata[0]}<br>' + 
+                  '<b>Origin</b>: %{customdata[2]}<extra></extra>')
 
 # Scatter plot for system
 fig_system = px.scatter(
@@ -189,4 +167,4 @@ def local_css(file_name):
 
 local_css("style.css")
 
-
+st.write("Copyright 2024 NitroVolt ApS. This tool is licensed under the GNU General Public Licenses v3.0.")
